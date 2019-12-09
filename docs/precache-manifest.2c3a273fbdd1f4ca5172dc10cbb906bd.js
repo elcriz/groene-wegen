c15e8ddd1066d276f965945749a4460dcd39acda
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2019-12-09 11:06:08 +0100

    Updates static docs files

--- docs/precache-manifest.2c3a273fbdd1f4ca5172dc10cbb906bd.js
+++ docs/precache-manifest.2c3a273fbdd1f4ca5172dc10cbb906bd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "756fe4f570805a9f455e0fe51d147e88",
-    "url": "/index.html"
+    "url": "/groene-wegen/docs/index.html"
   },
   {
     "revision": "a5ab44a3b2ace9a7438e",
-    "url": "/static/css/main.26fc2d8e.chunk.css"
+    "url": "/groene-wegen/docs/static/css/main.26fc2d8e.chunk.css"
   },
   {
     "revision": "4828aca9fe2116c37e53",
-    "url": "/static/js/2.db1b208b.chunk.js"
+    "url": "/groene-wegen/docs/static/js/2.db1b208b.chunk.js"
   },
   {
     "revision": "f032203ca460334c00de541c30a6078a",
-    "url": "/static/js/2.db1b208b.chunk.js.LICENSE"
+    "url": "/groene-wegen/docs/static/js/2.db1b208b.chunk.js.LICENSE"
   },
   {
     "revision": "a5ab44a3b2ace9a7438e",
-    "url": "/static/js/main.a6b12f4b.chunk.js"
+    "url": "/groene-wegen/docs/static/js/main.a6b12f4b.chunk.js"
   },
   {
     "revision": "4305ba5b745fb543f597",
