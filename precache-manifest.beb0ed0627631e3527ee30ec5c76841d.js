d5f8526d1e642b491dcdc23d13c8f663331ceb3c
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2019-12-09 11:11:37 +0100

    Updates

---
+++ precache-manifest.beb0ed0627631e3527ee30ec5c76841d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f18c658e4ff522e90dc1821659fd37",
+    "url": "/groene-wegen/index.html"
+  },
+  {
+    "revision": "a6f4097a216bc64d90cb",
+    "url": "/groene-wegen/static/css/main.26fc2d8e.chunk.css"
+  },
+  {
+    "revision": "4828aca9fe2116c37e53",
+    "url": "/groene-wegen/static/js/2.db1b208b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/groene-wegen/static/js/2.db1b208b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6f4097a216bc64d90cb",
+    "url": "/groene-wegen/static/js/main.7f79d922.chunk.js"
+  },
+  {
+    "revision": "8d28d8df3c6a2f486ace",
+    "url": "/groene-wegen/static/js/runtime-main.3b3df5af.js"
+  }
+]);
